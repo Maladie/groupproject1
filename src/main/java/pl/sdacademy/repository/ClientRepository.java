fbcce28630fcddf87b8adaa50907b87723fbed8d
Jakub <jakkabza@gmail.com>
2017-10-27 18:58:54 +0200

    interface methods added

--- src/main/java/pl/sdacademy/repository/ClientRepository.java
+++ src/main/java/pl/sdacademy/repository/ClientRepository.java
@@ -1,7 +1,14 @@
 package pl.sdacademy.repository;
 
-/**
- * Created by RENT on 2017-10-27.
- */
-public interface ClientRepository {
+import pl.sdacademy.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+
+    List<Client> findAll();
+    List<Client> findBySurname(String surname);
+    List<Client> findByAddressCity(String city);
 }
