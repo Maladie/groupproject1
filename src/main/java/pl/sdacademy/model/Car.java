0335ec77150c5fd851bdf5f09efc014af6ad2350
Bartek <wolniak.bartosz@gmail.com>
2017-10-27 18:45:07 +0200

    class car

---
+++ src/main/java/pl/sdacademy/model/Car.java
@@ -0,0 +1,79 @@
+package pl.sdacademy.model;
+
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+public class Car {
+
+    private Integer id;
+    @NotNull
+    private String company;
+    @NotNull
+    private String model;
+    @NotNull
+    @Min(0)
+    @Max(10000)
+    private Integer engineSize;
+    @NotNull
+    private String type;
+    @NotNull
+    private boolean isReserved;
+
+    public Car(String company, String model, Integer engineSize, String type) {
+        this.company = company;
+        this.model = model;
+        this.engineSize = engineSize;
+        this.type = type;
+        isReserved = false;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getEngineSize() {
+        return engineSize;
+    }
+
+    public void setEngineSize(Integer engineSize) {
+        this.engineSize = engineSize;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        isReserved = reserved;
+    }
+}
