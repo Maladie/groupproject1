138fd8a7d1a6f10ca9485c2ee006cc8fb20b00d9
Maladie <mariusz.maks.szymanski@gmail.com>
2017-10-27 18:44:51 +0200

    databaseconfig

---
+++ src/main/java/pl/sdacademy/repository/ClientDao.java
@@ -0,0 +1,19 @@
+package pl.sdacademy.repository;
+
+import org.springframework.stereotype.Repository;
+import pl.sdacademy.model.Client;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@javax.transaction.Transactional
+public class ClientDao {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void saveClient(Client client) {
+        entityManager.persist(client);
+    }
+}
--- src/main/java/pl/sdacademy/service/ClientService.java
+++ src/main/java/pl/sdacademy/service/ClientService.java
@@ -1,4 +1,20 @@
 package pl.sdacademy.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.sdacademy.repository.ClientDao;
+import pl.sdacademy.repository.ClientRepository;
+
+@Service
 public class ClientService {
+
+    private ClientDao clientDao;
+
+    private ClientRepository clientRepository;
+
+    @Autowired
+    public ClientService(ClientDao clientDao, ClientRepository clientRepository) {
+        this.clientDao = clientDao;
+        this.clientRepository = clientRepository;
+    }
 }
